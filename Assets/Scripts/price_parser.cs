683e239af08151dc4bc04cccf845158b84014cb3
Niko <andrew.nikolayenko@gmail.com>
2022-02-22 16:40:22 +0200

    upd

--- Assets/Scripts/price_parser.cs
+++ Assets/Scripts/price_parser.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Web;
 
 
 public class price_parser : MonoBehaviour
 {
-    public string url = "https://bankchart.com.ua/spravochniki/indikatory_rynka/tarify_na_gas/39";
+    public static string url = "https://rvgaszbut.com.ua/rates/ricnij-bazova-propozicia-7";
+    public static string search = "price__new-rate h3";
     public Text text;
     private void Start()
     {
+        
         StartCoroutine(Startc());
     }
     IEnumerator Startc()
@@ -17,6 +20,19 @@ public class price_parser : MonoBehaviour
         using (WWW www = new WWW(url))
         {
             yield return www;
+            int p = url.IndexOf(search);
+            if (p >= 0)
+        {
+            int start = p + search.Length;
+
+            int end = url.IndexOf("</span>", start);
+            if(end >= 0)
+            {
+                string v = url.Substring(start, end - start);
+                float value = float.Parse(v);
+                Debug.Log(value);
+            }
+        }
             text.text = www.text;
             Debug.Log(text.text);
         }
